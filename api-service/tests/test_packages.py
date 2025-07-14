856aa54655706e8b2a8693aa723dd6ffcee4369f
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2025-07-15 03:04:27 +0400

    🧹 api-service: refactor sql init file content

--- api-service/tests/test_packages.py
+++ api-service/tests/test_packages.py
@@ -5,7 +5,7 @@ from endpoints import *
 
 dolt_package = {'repo_name': 'repo1', 'pkg_name': 'dolt', 'pkg_version': '1.30.5-1.1', 'pkg_base': 'dolt', 'pkg_desc': 'Git for data! A version controlled relational database', 'pkg_groups': [], 'pkg_url': 'https://www.dolthub.com', 'pkg_license': ['Apache'], 'pkg_arch': 'x86_64', 'pkg_builddate': 1704385114, 'pkg_packager': 'CachyOS <admin@cachyos.org>', 'pkg_csize': 18527187, 'pkg_isize': 93412792, 'pkg_sha256sum': '36f26a9a520800f3a2c577f07334ca1c209bcd3ac6c457108abd417ec242d299', 'pkg_pgpsig': None, 'pkg_replaces': [], 'pkg_depends': ['glibc'], 'pkg_optdepends': [], 'pkg_makedepends': ['go'], 'pkg_checkdepends': [], 'pkg_conflicts': [], 'pkg_provides': [], 'pkg_files': 'usr/,usr/bin/,usr/bin/dolt'.split(','), 'updated': 1751364311}
 
-@pytest.mark.pgsql('db_1', files=['initial_data.sql'])
+@pytest.mark.pgsql('init_db', files=['initial_data.sql'])
 async def test_package(service_client):
     response = await get_package(service_client)
     assert response.status == 404
@@ -26,7 +26,7 @@ async def test_package(service_client):
     assert response.status == 200
     assert response.json()['package'] == dolt_package
 
-@pytest.mark.pgsql('db_1', files=['initial_data.sql'])
+@pytest.mark.pgsql('init_db', files=['initial_data.sql'])
 async def test_search_packages(service_client):
     # invalid usage
     response = await search_packages(service_client, 0, 0)

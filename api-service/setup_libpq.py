813eb375d08eb2dcc73bbd03a68e418818754a98
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2025-10-10 23:54:16 +0400

    ♻  api-service: update postgres

--- api-service/setup_libpq.py
+++ api-service/setup_libpq.py
@@ -35,7 +35,7 @@ def main():
         pathlib.Path(SRC_PATH + "/third_party").mkdir(parents=True, exist_ok=True)
 
     if not os.path.exists(SRC_PATH + "/third_party/postgresql"):
-        res = subprocess.run(["git", "clone", "--depth", "1", "--branch", "REL_17_4", "https://github.com/postgres/postgres.git", SRC_PATH + "/third_party/postgresql"])
+        res = subprocess.run(["git", "clone", "--depth", "1", "--branch", "REL_18_0", "https://github.com/postgres/postgres.git", SRC_PATH + "/third_party/postgresql"])
         if res.returncode:
             print("Failed to clone repository")
             exit(1)
@@ -59,6 +59,7 @@ def main():
             "-Dpltcl=disabled",
             "-Dpam=disabled",
             "-Dldap=disabled",
+            "-Dlibcurl=disabled",
             "-Dsystem_tzdata=/usr/share/zoneinfo",
             "-Duuid=e2fs",
             "-Dlz4=enabled",

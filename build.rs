12375fa0b32bd2710e68d54a0175e780fb3b6a50
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-06-29 17:53:13 +0400

    build.rs: inline args

--- build.rs
+++ build.rs
@@ -11,12 +11,7 @@ fn main() {
     let mut command = Cli::command();
     for shell in [Shell::Bash, Shell::Fish, Shell::Zsh] {
         generate_to(shell, &mut command, "repo-manage-util", &out_path).unwrap_or_else(|err| {
-            panic!(
-                "Couldn't generate completion for shell {:?} in {}: {}",
-                shell,
-                out_path.display(),
-                err
-            )
+            panic!("Couldn't generate completion for shell {shell:?} in {out_path:?}: {err}")
         });
     }
 }
